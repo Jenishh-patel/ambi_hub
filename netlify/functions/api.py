a5360f4fa5ece5a566543ee1fae026a1cf261b39
Jenishh-patel <vip573799@gmail.com>
2025-11-18 23:21:53 +0530

    Use awsgi to route Netlify functions to Django WSGI; add awsgi to requirements

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,51 +1,27 @@
 import sys
 import os
 import django
-from django.conf import settings
-from django.core.wsgi import get_wsgi_application
 
 # Add the project directory to the Python path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Configure Django settings
+# Point to Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ambitioushub.settings')
 
 # Setup Django
 django.setup()
 
-from django.test.client import Client
+from ambitioushub.wsgi import application
+import awsgi
 
 def handler(event, context):
+    """Netlify function handler that forwards the incoming event to Django WSGI app
+
+    This uses awsgi to convert the Netlify (Lambda-like) event into a WSGI request
+    and returns a Lambda-style response expected by Netlify functions.
     """
-    Netlify Function handler for Django API requests
-    Routes all /api/* requests through Django's URL routing
-    """
-    
-    client = Client()
-    path = event.get('path', '/')
-    method = event.get('httpMethod', 'GET').upper()
-    body = event.get('body', '')
-    headers = event.get('headers', {})
-    
     try:
-        # Route the request through Django
-        if method == 'GET':
-            response = client.get(path, HTTP_AUTHORIZATION=headers.get('authorization', ''))
-        elif method == 'POST':
-            response = client.post(path, data=body, HTTP_AUTHORIZATION=headers.get('authorization', ''))
-        elif method == 'PUT':
-            response = client.put(path, data=body, HTTP_AUTHORIZATION=headers.get('authorization', ''))
-        elif method == 'DELETE':
-            response = client.delete(path, HTTP_AUTHORIZATION=headers.get('authorization', ''))
-        else:
-            response = client.get(path)
-        
-        return {
-            'statusCode': response.status_code,
-            'body': response.content.decode('utf-8'),
-            'headers': dict(response.items())
-        }
-    
+        return awsgi.response(application, event, context)
     except Exception as e:
         return {
             'statusCode': 500,
